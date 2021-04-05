d33286cc495afdc260d562be2486020591e58b16
Dyukov Vlad <dyukovlad@gmail.com>
2021-04-05 17:42:46 +0300

    add new change

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ connection.query(
           tabId: users[i].TABID,
           date: moment(users[i].LOGTIME).format('D.MM.YYYY'),
           temp: parseInt(cell, 16) / 10,
+          podpis: ' ',
+          fioPodpis: ' ',
         });
       }
     }
@@ -55,13 +57,25 @@ const toExcel = (data) => {
   let workbook = new Excel.Workbook();
   let worksheet = workbook.addWorksheet('Журнал');
 
-  worksheet.mergeCells('A1:h1');
-  worksheet.getRow(1).values = [
-    'Журнал регистрации измерения температуры работников для профилактики коронавируса',
-  ];
+  const headerStyle = {
+    vertical: 'middle',
+    horizontal: 'center',
+    wrapText: true,
+  };
+
+  worksheet.mergeCells('A1', 'H1');
+  worksheet.mergeCells('A2', 'H2');
+
+  worksheet.getCell(
+    'A1'
+  ).value = `Общество с ограниченной  ответственностью "Альфа"
+  ООО "Альфа"`;
+
+  worksheet.getCell('A2').value =
+    'Журнал регистрации измерения температуры работников для профилактики коронавируса';
 
   // header
-  const title = worksheet.getRow(2);
+  const title = worksheet.getRow(3);
 
   title.values = [
     '№ п/п',
@@ -85,17 +99,18 @@ const toExcel = (data) => {
     { key: 'fioPodpis', width: 15 },
   ];
 
-  worksheet.getRow(2).alignment = {
-    vertical: 'middle',
-    horizontal: 'center',
-    wrapText: true,
-  };
+  worksheet.getRow(1).alignment = headerStyle;
+  worksheet.getRow(1).height = 50;
+  worksheet.getRow(2).alignment = headerStyle;
+  worksheet.getRow(2).height = 50;
+  worksheet.getRow(3).alignment = headerStyle;
 
   // worksheet.columns.forEach((column) => {
   //   column.width = column.header.length < 12 ? 12 : column.header.length;
   // });
 
-  worksheet.getRow(2).font = { bold: true };
+  // worksheet.getRow(1).font = { italic: true };
+  worksheet.getRow(3).font = { bold: true };
 
   data.forEach((e, index) => {
     // row 1 is the header.
@@ -106,7 +121,45 @@ const toExcel = (data) => {
     });
   });
 
+  worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+    row.eachCell({ includeEmpty: false }, function (cell, colNumber) {
+      cell.border = {
+        top: { style: 'thick' },
+        left: { style: 'thick' },
+        bottom: { style: 'thick' },
+        right: { style: 'thick' },
+      };
+    });
+  });
+
+  // autoWidth(worksheet);
+
+  // worksheet.columns.forEach((column) => {
+  //   column.border = {
+  //     top: { style: 'thick' },
+  //     left: { style: 'thick' },
+  //     bottom: { style: 'thick' },
+  //     right: { style: 'thick' },
+  //   };
+  // });
+
   workbook.xlsx.writeFile('excel.xlsx');
+};
+
+const autoWidth = (worksheet, minimalWidth = 10) => {
+  worksheet.columns.forEach((column) => {
+    if (column._number !== 1 && column._number !== 8) {
+      let maxColumnLength = 0;
+      column.eachCell({ includeEmpty: true }, (cell) => {
+        maxColumnLength = Math.max(
+          maxColumnLength,
+          minimalWidth,
+          cell.value ? cell.value.toString().length : 0
+        );
+      });
+      column.width = maxColumnLength + 2;
+    }
+  });
 };
 
 connection.end(function (err) {
